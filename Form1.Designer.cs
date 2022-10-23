64bc11c9ed661a4ea439a632e8310e95b2de2860
Your Name <you@example.com>
2022-10-23 21:45:22 +0700

    CT PN AND VIDEO

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CT_NGHE_NHAC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bt_thoat = new System.Windows.Forms.Button();
+            this.bt_open_click = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(466, 325);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(508, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 264);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Danh Sách";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Đang phát";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 368);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // bt_thoat
+            // 
+            this.bt_thoat.BackColor = System.Drawing.Color.LawnGreen;
+            this.bt_thoat.Location = new System.Drawing.Point(687, 352);
+            this.bt_thoat.Name = "bt_thoat";
+            this.bt_thoat.Size = new System.Drawing.Size(75, 53);
+            this.bt_thoat.TabIndex = 5;
+            this.bt_thoat.Text = "Thoát";
+            this.bt_thoat.UseVisualStyleBackColor = false;
+            this.bt_thoat.Click += new System.EventHandler(this.bt_thoat_Click);
+            // 
+            // bt_open_click
+            // 
+            this.bt_open_click.BackColor = System.Drawing.SystemColors.Control;
+            this.bt_open_click.Location = new System.Drawing.Point(498, 368);
+            this.bt_open_click.Name = "bt_open_click";
+            this.bt_open_click.Size = new System.Drawing.Size(75, 37);
+            this.bt_open_click.TabIndex = 5;
+            this.bt_open_click.Text = ". . .";
+            this.bt_open_click.UseVisualStyleBackColor = false;
+            this.bt_open_click.Click += new System.EventHandler(this.bt_open_click_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_open_click);
+            this.Controls.Add(this.bt_thoat);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button bt_thoat;
+        private System.Windows.Forms.Button bt_open_click;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CT_NGHE_NHAC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog openFileDialog;
+        string[] filePaths;
+        string[] fileNames;
+        private void bt_open_click_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Mp3 files mp4 files(*.mp3 , *.mp4)|*.mp*";
+            openFileDialog.Title = "open";
+            openFileDialog.Multiselect = true;
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePaths = openFileDialog.FileNames;
+                fileNames = openFileDialog.SafeFileNames;
+            }
+            foreach(var item in fileNames)
+            {
+                this.listBox1.Items.Add(item);
+            }
+        }
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex != -1)
+            {
+                int choose = listBox1.SelectedIndex;
+                axWindowsMediaPlayer1.URL = filePaths[choose];
+                this.textBox1.Text = fileNames[choose];
+            }
+        }
+        private void bt_thoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát không ?" , "Thông báo" , MessageBoxButtons.YesNo , MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            this.Close();
+        }
+    }
+}
